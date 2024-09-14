8c40b205e743ef4b35088e268eba72bd2dfb85cb
ydaniju <yusuf.daniju@doctolib.com>
2024-09-14 20:12:51 +0200

    initialize main with winston

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require("electron");
+const { createLogger, format, transports } = require("winston");
+
+const getTransports = () => {
+  const baseTransports = [
+    new transports.File({
+      filename: `${app.getPath("appData")}/error.log`,
+      level: "error",
+    }),
+    new transports.File({ filename: `${app.getPath("appData")}/ssshh.log` }),
+  ];
+
+  if (process.env.NODE_ENV !== "production") {
+    baseTransports.push(
+      new transports.Console({
+        format: format.simple(),
+      })
+    );
+  }
+
+  return baseTransports;
+};
+
+const logger = createLogger({
+  level: "info",
+  format: format.json(),
+  defaultMeta: { service: "MainWindow" },
+  transports: getTransports(),
+});
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  logger.info("Main app has started");
+});
