f65fc9bdf0820af56315281c0d8c1ccdc6041dda
ydaniju <yusuf.daniju@doctolib.com>
2024-09-14 21:21:56 +0200

    add preloads

--- main.js
+++ main.js
@@ -1,43 +1,47 @@
-const { app, BrowserWindow } = require("electron");
-const { createLogger, format, transports } = require("winston");
+const { app, BrowserWindow } = require('electron')
+const { createLogger, format, transports } = require('winston')
+const path = require('node:path')
 
 const getTransports = () => {
   const baseTransports = [
     new transports.File({
-      filename: `${app.getPath("appData")}/error.log`,
-      level: "error",
+      filename: `${app.getPath('appData')}/error.log`,
+      level: 'error',
     }),
-    new transports.File({ filename: `${app.getPath("appData")}/ssshh.log` }),
-  ];
+    new transports.File({ filename: `${app.getPath('appData')}/ssshh.log` }),
+  ]
 
-  if (process.env.NODE_ENV !== "production") {
+  if (process.env.NODE_ENV !== 'production') {
     baseTransports.push(
       new transports.Console({
         format: format.simple(),
       })
-    );
+    )
   }
 
-  return baseTransports;
-};
+  return baseTransports
+}
 
 const logger = createLogger({
-  level: "info",
+  level: 'info',
   format: format.json(),
-  defaultMeta: { service: "MainWindow" },
+  defaultMeta: { service: 'MainWindow' },
   transports: getTransports(),
-});
+})
 
 const createWindow = () => {
   const win = new BrowserWindow({
     height: 600,
     width: 800,
-  });
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
 
-  win.loadFile("index.html");
-};
+  win.loadFile('index.html')
+}
 
 app.whenReady().then(() => {
-  createWindow();
-  logger.info("Main app has started");
-});
+  createWindow()
+  logger.info('Main app has started')
+})
